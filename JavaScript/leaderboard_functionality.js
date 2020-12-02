d22094b4a5e3654a1f5a533b20795646f1fb47a8
orion13579 <sloren@my.bcit.ca>
2020-12-02 15:10:02 -0800

    Updated leaderboard to use fake database

--- JavaScript/leaderboard_functionality.js
+++ JavaScript/leaderboard_functionality.js
@@ -1,9 +1,3 @@
-// let users = require('../Database Fake/database');
-let isGlobalLeaderboard = true;
-let currentLeaderboard = 0
-let possibleLeaderboards = ["overallButton", "ticTacToeButton", "hangmanButton", "sudokuButton", "connect4Button", "rpsButton", "pokerButton", 
-"blackjackButton", "wordSearchButton", "snakeButton", "pacmanButton", "froggerButton", "cookieClickerButton"]
-
 function selectAButton(buttonElement){
     let listOfButtons = buttonElement.parentNode.parentNode.querySelectorAll("button");
     for (let buttonIndex = 0; buttonIndex < listOfButtons.length; buttonIndex++){
@@ -61,33 +55,6 @@ document.getElementById("wordSearchButton").addEventListener('click', function (
     selectAButton(button);
 });
 
-function setGlobalOrLocalLeaderboard(buttonID){
-    let buttonReference = ["globalButton", "classButton"]
-    if (isGlobalLeaderboard && buttonID == 1){
-        document.getElementById(buttonReference[0]).style.backgroundColor = "whiteSmoke";
-        document.getElementById(buttonReference[1]).style.backgroundColor = "lavender";
-        isGlobalLeaderboard = false;
-    }
-    else{
-        document.getElementById(buttonReference[0]).style.backgroundColor = "lavender";
-        document.getElementById(buttonReference[1]).style.backgroundColor = "whiteSmoke";
-        isGlobalLeaderboard = true;
-    }
-}
-
-function getLeaderboard(leaderboardReference){
-    if (isGlobalLeaderboard){
-        if (currentLeaderboard != leaderboardReference){
-            document.getElementById(possibleLeaderboards[leaderboardReference]).style.backgroundColor = "lavender";
-            document.getElementById(possibleLeaderboards[currentLeaderboard]).style.backgroundColor = "whiteSmoke";
-            currentLeaderboard = leaderboardReference;
-        }
-    }
-    else{
-        if (currentLeaderboard != leaderboardReference){
-            document.getElementById(possibleLeaderboards[leaderboardReference]).style.backgroundColor = "lavender";
-            document.getElementById(possibleLeaderboards[currentLeaderboard]).style.backgroundColor = "whiteSmoke";
-            currentLeaderboard = leaderboardReference;
-        }
-    }
+for (let i = 0; i < users.length; i++){
+    console.log(users[i].username)
 }
